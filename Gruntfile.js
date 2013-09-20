1f61ba5fb2612f721abd6aea695ef70036dbfa6c
Caio Gondim <me@caiogondim.com>
2013-09-20 16:20:59 -0300

    Adds grunt task 'jshint'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
 
   grunt.initConfig({
     uglify: {
@@ -11,6 +12,31 @@ module.exports = function(grunt) {
         files: {
           './lib/konami-code.min.js': './lib/konami-code.js'
         }
+      }
+    },
+    jshint: {
+      files: ['lib/konami-code.js'],
+      options: {
+        camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        indent: 2,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        plusplus: true,
+        quotmark: 'single',
+        undef: true,
+        unused: true,
+        strict: true,
+        trailing: true,
+        maxparams: 3,
+        maxlen: 80,
+        asi: true
       }
     }
   })
