05a3a4dd902d709736dd6b9692ea219d810f21ba
Caio Gondim <me@caiogondim.com>
2013-09-25 00:50:48 -0300

    Adds 'grunt-notify' task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-notify')
 
   grunt.initConfig({
     watch: {
