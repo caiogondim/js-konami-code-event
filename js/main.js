a03180bb91b6bd84b2897ad7d725d60980b3bd13
Caio Gondim <me@caiogondim.com>
2013-10-19 17:04:34 -0300

    Refactoring

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var main = (function() {
+  'use strict';
+
+  var main = {
+    init: function() {
+      this._bindEvents()
+      this._initInstructionsBlinkInterval()
+
+      return this
+    }
+
+  , _bindEvents: function() {
+      document.body.addEventListener('konamiCode', this.showToasty)
+    }
+
+  , _initInstructionsBlinkInterval: function() {
+      // Blinks the instructions
+      var instructions = document.querySelector('.instructions')
+      instructions.style.visibility = 'visible'
+      setInterval(function () {
+        if (instructions.style.visibility == 'visible') {
+          instructions.style.visibility = 'hidden'
+        } else if (instructions.style.visibility == 'hidden') {
+          instructions.style.visibility = 'visible'
+        }
+      }, 500)
+    }
+
+  , showToasty: function() {
+      var toasty = document.querySelector('.toasty')
+      toasty.classList.add('is-visible')
+
+      setTimeout(function () {
+        document.querySelector('.toasty-sound').play()
+      }, 300)
+
+      setTimeout(function () {
+        toasty.classList.remove('is-visible')
+      }, 1000)
+    }
+  }
+
+  return main.init()
+
+}())
