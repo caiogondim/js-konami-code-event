a2fc9efa76108924a849be40d732984138b9a322
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2013-10-19 22:58:39 -0300

    Tests updated

--- test/spec.js
+++ test/spec.js
@@ -15,6 +15,10 @@ describe('Event konami code', function() {
     document.dispatchEvent(keyboardEvent);
   };
 
+  beforeEach(function() {
+    wasEventFired = false;
+  })
+
   it('should fire the event when someone presses the konami code', function() {
 
     [38,38,40,40,37,39,37,39,66,65].forEach(function(val) {
@@ -24,7 +28,28 @@ describe('Event konami code', function() {
     expect(wasEventFired).toEqual(true)
   });
 
+  it("Don't fire the event because the sequence is incorrect" , function(){
+    [38,38,40,40,37,39,37,39,66,66].forEach(function(val) {
+        hitKey(val);
+    });
+    expect(wasEventFired).toEqual(false)
+  });
+
   it("Don't fire the event because the delay is higger than 1.5s" , function(){
 
-  })
+    var keys = [38,38,40,40,37,39,37,39,66,65];
+    waitsFor(function() {
+        
+      var interval = setInterval(function() {
+        hitKey(keys.shift());
+        if(keys.length == 0) clearInterval(interval);
+      }, 2000);
+
+      return keys.length == 0;
+    });
+
+    runs(function() {
+      expect(wasEventFired).toEqual(false);
+    })
+  });
 })
