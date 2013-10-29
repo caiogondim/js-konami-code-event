5e3b217906e4bb40724f7879e4f675e10b459c53
Breno Polanski <breno.polanski@gmail.com>
2013-10-29 19:52:42 -0200

    Removido função _initInstructionsBlinkInterval
    Removido função _initInstructionsBlinkInterval, para adicionar @keyframes blink na folha de estilo.

--- js/main.js
+++ js/main.js
@@ -4,26 +4,12 @@ var main = (function() {
   var main = {
     init: function() {
       this._bindEvents()
-      this._initInstructionsBlinkInterval()
 
       return this
     }
 
   , _bindEvents: function() {
       document.addEventListener('konamiCode', this.showToasty)
-    }
-
-  , _initInstructionsBlinkInterval: function() {
-      // Blinks the instructions
-      var instructions = document.querySelector('.instructions')
-      instructions.style.visibility = 'visible'
-      setInterval(function () {
-        if (instructions.style.visibility == 'visible') {
-          instructions.style.visibility = 'hidden'
-        } else if (instructions.style.visibility == 'hidden') {
-          instructions.style.visibility = 'visible'
-        }
-      }, 500)
     }
 
   , showToasty: function() {
